36f7aaafdfd19b9282f612c8bf8255272bf35dad
BorisRouge <you@example.com>
2022-11-23 23:11:19 +0700

    basic checkout and subsequent redirect

--- orders/urls.py
+++ orders/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Item.as_view(), name='orders'),  # TODO: alternative names: catalogue, view, checkout.
-]
+    path('', views.Item.as_view()),  # TODO: alternative names: catalogue, view, checkout.
+    path('create-checkout-session', views.create_checkout_session),
+    path('success', views.redirect_after_transaction),
+    path('cancel', views.redirect_after_transaction),
+
+    ]
--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+import os
+from django.shortcuts import render, redirect, HttpResponse, HttpResponseRedirect
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+import stripe
+
 
 # Create your views here.
 
@@ -7,3 +11,30 @@ from django.views import View
 class Item(View):  # TODO: see todo in urls.
     def get(self, request):
         return render(request, template_name="orders/page.html")
+
+
+@csrf_exempt
+def create_checkout_session(request):
+    stripe.api_key = os.getenv('stripe_api_key')
+    session = stripe.checkout.Session.create(
+        line_items=[{'price_data': {
+            'product_data': {'name': 'employee'},
+            "unit_amount": f"{request.POST.get('item_1')}",
+            "currency": 'RUB'},
+            "quantity": 1,
+        }],
+        mode='payment',
+        success_url='http://localhost:8000/success',
+        cancel_url='http://localhost:8000/cancel',
+    )
+    return redirect(session.url, code=303)
+
+
+def redirect_after_transaction(request):
+    options = {'/success': 'Платеж успешно совершен.',
+               '/cancel': 'Платеж отменен.', }
+    return render(request,
+                  template_name='orders/redirect_after_transaction.html',
+                  context={'text': options[request.get_full_path()]})
+
+
